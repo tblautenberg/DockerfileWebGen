bb4928b48c827e3a584afda56e8ba724b4c76934
Tobias Blautenberg <tobiasglerupnielsen1994@gmail.com>
2024-04-25 13:59:01 +0200

    Added logo and css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import DockerfileGenerator from './DockerfileGenerator';
 import ParticlesComponent from './components/particles';
+import dockerLogo from './Images/docker.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Dockerfile Generator</h1>
-      </header>
-      <div className="container">
-        <DockerfileGenerator className="dockerfile-generator" />
-        <ParticlesComponent id="particles" />
-      </div>
+<div className="App">
+  <header className="App-header">
+    <div className="logo-container">
+      <img src={dockerLogo} className="App-logo" alt="Docker Logo" />
+      <h1> Dockerfile Generator &nbsp; &nbsp;</h1>
     </div>
+    <DockerfileGenerator className="dockerfile-generator" />
+  </header>
+  <div className="container">
+    <ParticlesComponent id="particles" />
+  </div>
+</div>
+
   );
 }
 
